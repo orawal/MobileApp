98e042ad3e723756012f70c44deef9ccf16acd1b
Oshin <oshin.rawal94@gmail.com>
2016-08-01 22:26:32 -0400

    Product list

---
+++ app/src/main/java/com/example/smartmarket/DataHelper.java
@@ -0,0 +1,14 @@
+package com.example.smartmarket;
+
+import org.json.JSONArray;
+
+/**
+ * Created by oshinrawal on 8/1/16.
+ */
+public class DataHelper {
+    protected void readJson() {
+        String url = "http://ec2-52-87-235-234.compute-1.amazonaws.com:8080/searchProducts?Keywords=Harry&SearchIndex=All";
+        JSONArray dataJsonArr = null;
+
+    }
+}
